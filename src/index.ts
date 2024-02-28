ed8bd854259aa4c4b94d780be60fe8d6aa858d1b
aspcodenet <stefan.holmberg@systementor.se>
2024-02-28 09:27:07 +0100

    first commit

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+// int  test = 0; // fALSE
+// int  test = 1; // TRUE
+// bool test = true;
+// bool test = false;
+
+
+
+import express  from "express";
+import type {Express}  from "express";
+
+
+
+
+const app: Express = express();
+
