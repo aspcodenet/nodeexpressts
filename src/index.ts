8a7ed81e3ce42e7c197730266767b0db47e4b853
aspcodenet <stefan.holmberg@systementor.se>
2024-02-28 10:34:35 +0100

    db

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,34 @@
-// int  test = 0; // fALSE
-// int  test = 1; // TRUE
-// bool test = true;
-// bool test = false;
-
-
-
 import express  from "express";
-import type {Express}  from "express";
+import type {Express,Request,Response}  from "express";
+import  {getAllMessages}  from "./models/message";
+import { initializeDatabase, sequelize } from "./database/db";
 
-
-
-
+const port:Number = 3000
 const app: Express = express();
+
+app.get("/", (req: Request, res: Response) => {
+    res.send("Express + TypeScript Server2");
+});
+
+app.get("/api/messages", (req: Request, res: Response) => {
+
+    // let a : Number | null  = 100
+    // a = 200
+    // a = null
+
+    res.json(getAllMessages())
+});
+
+
+
+
+app.listen(port, async () => {
+    await initializeDatabase()
+    
+    await sequelize.sync()  // DROP TABLES, CREATE TABLES
+    // kör en gång och sen  bortkommentera
+
+    console.log(`[server]: Server is running at http://localhost:${port}`);
+}); 
+
 
