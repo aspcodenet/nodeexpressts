448a8fc9b73eb02c74cd65b6c5dad7e34233ac50
aspcodenet <stefan.holmberg@systementor.se>
2024-03-11 10:20:54 +0100

    v2

--- src/database/player.ts
+++ src/database/player.ts
@@ -1,5 +1,7 @@
 import { Model, CreationOptional,InferAttributes, InferCreationAttributes } from 'sequelize';
  
+
+// NULÄGE !!! Vi vill att NU ska alla Players ha dessa properties
   export class Player extends Model<InferAttributes<Player, {  }>, InferCreationAttributes<Player, {  }>> {
     declare id: CreationOptional<number>;
     declare name: string;
--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,16 @@ import express  from "express";
 import type {Express,Request,Response}  from "express";
 import  {getAllMessages}  from "./models/message";
 import { initializeDatabase, sequelize } from "./database/db";
+import { migrate } from "./database/migrations/migrationhelper";
+
 
 const port:Number = 3000
 const app: Express = express();
 
+
 app.get("/", (req: Request, res: Response) => {
+
+
     res.send("Express + TypeScript Server2");
 });
 
@@ -35,9 +40,9 @@ app.get("/api/messages", (req: Request, res: Response) => {
 
 
 app.listen(port, async () => {
-    await initializeDatabase()
-    
-    await sequelize.sync()  // DROP TABLES, CREATE TABLES
+    //await initializeDatabase()
+    await migrate(sequelize)
+    //await sequelize.sync()  // DROP TABLES, CREATE TABLES
     // kör en gång och sen  bortkommentera
 
     console.log(`[server]: Server is running at http://localhost:${port}`);
